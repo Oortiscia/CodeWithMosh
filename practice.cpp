84ae0ef3547d5f6de376d00dc56ecf64cd770f41
unknown <fshz1382@gmail.com>
2025-06-04 04:26:40 +0330

    progmess

--- practice.cpp
+++ practice.cpp
@@ -1,20 +1,20 @@
 
 // dice.
-/* #include "iostream"
-#include "cstdlib" // random function.
-#include "ctime"   // for time function.elapses time in terms of seconds from jan 1st 1976.
-using namespace std;
-int main()
-{
+/* #include "cstdlib" // random function.
+#include "ctime"   // for time function.elapses time in terms of seconds from
+#include "iostream"
+
+jan 1st 1976. using namespace std;
+int main() {
   const short minValue = 1;
   const short maxValue = 6;
 
   srand(time(nullptr)); // seed for rand().
-  short dice1 = (rand() % (maxValue - minValue + 1)) + 1; // limiting the range of random numbers that we get.
+  short dice1 = (rand() % (maxValue - minValue + 1)) + 1;
   short dice2 = (rand() % (maxValue - minValue + 1)) + 1;
+  // limiting the range of numbers.
 
-  cout << dice1 << endl
-       << dice2;
+  cout << dice1 << endl << dice2;
 } */
 
 // boolalpha.
@@ -43,8 +43,8 @@ int main()
   cout << setw(12) << "spring" << setw(10) << "nice" << endl // set with.
        << setw(12) << "summer" << setw(10) << "hot" << endl;
 
-  cout << left; // left is a sticky manipulator and it won't go away until we change it.
-  cout << setw(12) << "spring" << setw(10) << "nice" << endl
+  cout << left; // left is a sticky manipulator and it won't go away until we
+change it. cout << setw(12) << "spring" << setw(10) << "nice" << endl
        << setw(12) << "summer" << setw(10) << "hot" << endl;
 
   cout << 12.12345678 << endl;
@@ -53,7 +53,8 @@ int main()
        << 12.123456 << endl;
 
   cout << setprecision(10)
-       << 12.12345678 << endl; // both setprecision and fixed are sticky manipulators.
+       << 12.12345678 << endl; // both setprecision and fixed are sticky
+manipulators.
 } */
 
 // formatting.
@@ -103,14 +104,14 @@ int main()
   cout << "\n"
        << street << endl
        << cityName << ", " << state << ", " << zipcode;
-  // mosh says getline function is included in std namespace, my code compiles properly but has an unsolved error.
+  // mosh says getline function is included in std namespace, my code compiles
+properly but has an unsolved error.
 } */
 
 // numeric limits.
 /* #include "iostream"
-#include "limits" //mosh didn't use any library for the numeric_limits but i needed to.
-using namespace std;
-int main()
+#include "limits" //mosh didn't use any library for the numeric_limits but i
+needed to. using namespace std; int main()
 {
   cout << numeric_limits<int>::max() << endl
        << sizeof(int);
@@ -122,8 +123,8 @@ using namespace std;
 int main()
 {
   string names[3];        // hexadecimal num is the address of array in memory.
-  getline(cin, names[0]); // can also initialize this way: string names[3] = { [0]:john, marry, jack};.
-  getline(cin, names[1]); // index is from 0 to size-1,
+  getline(cin, names[0]); // can also initialize this way: string names[3] = {
+[0]:john, marry, jack};. getline(cin, names[1]); // index is from 0 to size-1,
   getline(cin, names[2]);
   cout << "\n"
        << names[0] << endl
@@ -138,7 +139,8 @@ int main()
 
 // C++ casting.
 /*
-==>   int a = 10 , int b = 3; double z = static_cast<double>(a) / b; (type conversions).
+==>   int a = 10 , int b = 3; double z = static_cast<double>(a) / b; (type
+conversions).
 */
 
 // order of operators.
@@ -171,7 +173,8 @@ int main()
   double yeasrsOfExperienc;
   string isUsCitizen;
   string hasBachelorDegree;
-  bool isUsCitizen0, hasBecholarDegree0, yeasrsOfExperienc0; // variables with 0 in the notation are boolean.
+  bool isUsCitizen0, hasBecholarDegree0, yeasrsOfExperienc0; // variables with 0
+in the notation are boolean.
 
   cout << "are you a US citizen? (yes/no) ";
   cin >> isUsCitizen;
@@ -216,8 +219,8 @@ int main()
 {
   int variable, inputVariable0, inputVariable1;
   cin >> inputVariable0 >> inputVariable1; // in case true     in case false
-  variable = (inputVariable1 > inputVariable0) ? inputVariable1 : inputVariable0;
-  cout << variable;
+  variable = (inputVariable1 > inputVariable0) ? inputVariable1 :
+inputVariable0; cout << variable;
 } */
 
 // switch case.
@@ -297,7 +300,8 @@ using namespace std;
 int main()
 {
   int numbers[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 234, 31, 2, 43};
-  int sizeOfArray = sizeof(numbers) / sizeof(int); // number of bytes this array takes in memory.
+  int sizeOfArray = sizeof(numbers) / sizeof(int); // number of bytes this array
+takes in memory.
 
   for (int i = 0; i <= (sizeOfArray - 1); i++)
     cout << numbers[i] << endl;
@@ -382,3 +386,97 @@ int main()
   }
   return 1;
 } */
+
+// do-while loops.
+/* #include "iostream"
+using namespace std;
+int main()
+{
+  int number;
+  do    // always gets executed atleast once.
+  {
+    cout << "number: ";
+    cin >> number;
+  } while (number < 1 || number > 5); // receive a number between 1 to 5.
+} */
+
+// continue statement.
+/* #include "iostream"
+using namespace std;
+int main()
+{
+  for (int i = 1; i <= 20; i += 1)
+    if (i % 3 != 0) {
+      continue;}
+    else {
+      cout << i <<endl;}
+} */
+
+// nested loops.
+/* #include "iostream"
+using namespace std;
+int main() {
+  int row;
+  cout << "number of rows: ";
+  cin >> row;
+
+  for (int i = 1; i <= row; i++) {
+
+    for (int j = 0; j < i; j++) {
+      cout << i << " " << j;
+    }
+
+    cout << endl;
+  }
+  return 1;
+} */
+
+// basic functions.
+/* #include "iostream"
+using namespace std;
+string fullName(string first, string last) { return first + " " + last; }
+void greet(string name) { cout << "hello " << name << endl; }
+int main() { greet(fullName("farham", "shakeri")); } */
+
+// a function for length caculation. (skips spaces)
+/* void length(string name) { // these values are parameters.
+  int j = 0;
+  int k = 0;
+  for (int i : name) {
+    if (+i == 32) // skiping spaces.
+      continue;
+    else
+      j += 1;
+  }
+  int length = j;
+  cout << length;
+} */
+
+// another basic function. (number comparison)
+/* #include "iostream"
+using namespace std;
+
+int maxNum(int firstNum, int secondNum) {
+  return (firstNum > secondNum) ? firstNum : secondNum; // conditional
+opperator.
+}
+int main() {
+  cout << maxNum(2, 1);
+} */
+
+// beacuse why not.
+/* #include "cstdlib"
+#include "ctime"
+#include "iostream"
+
+using namespace std;
+void dice() {
+  const int maxValue = 6;
+  const int minValue = 1;
+  srand(time(nullptr));
+  int firstDice = (rand() % (maxValue - minValue + 1)) + 1;
+  int secondDice = (rand() % (maxValue - minValue + 1)) + 1;
+  cout << firstDice << " and " << secondDice;
+}
+
+int main() { dice(); } */
