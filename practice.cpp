24ad3aacd9d7c344c21ff1100a2dff70fca7ec01
unknown <fshz1382@gmail.com>
2025-06-07 11:37:10 +0330

    still going

--- practice.cpp
+++ practice.cpp
@@ -568,6 +568,28 @@ int main() {
   hello::oddNums(24);
 } */
 
-#include "iostream"
-using std::cout;
-int main() { cout << "end of part 1"; }
+// arrays (again).
+/* #include "iostream"
+using namespace std;
+
+int main() {
+  int numbers[] = {10, 20, 30};
+
+  for (int i = 0; i < size(numbers); i += 1) {
+    // size function (std), returns array size in bytes.
+    cout << numbers[i] << endl;
+  }
+} */
+
+// playing with stars.
+/* #include "iostream"
+using namespace std;
+
+void star(int number) {
+  for (int i = 1; i <= number; i += 1) {
+    cout << endl;
+    for (int j = 0; j < i; j += 1) {
+      cout << "*";
+    }
+  }
+} */
